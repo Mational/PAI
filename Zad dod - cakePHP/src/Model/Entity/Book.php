6d6ea0460e2a98689f4f8f147a79832961201fd0
Mational <66531782+Mational@users.noreply.github.com>
2023-03-19 13:38:23 +0100

    Zadanie z cakePHP

---
+++ Zad dod - cakePHP/src/Model/Entity/Book.php
@@ -0,0 +1,13 @@
+<?php
+    namespace App\Model\Entity;
+    use Cake\ORM\Entity;
+    class Book extends Entity {
+        protected $_accessible = [
+            '*' => false,
+            'id' => false,
+            'title' => true,
+            'author' => true,
+            'genre' => true
+        ];
+    }
+?>
